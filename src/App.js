1789600337cdcfbab162007228add96307bce969
SylphidZoul <contact@jcruzcardozo.com>
2021-04-10 18:16:50 -0300

    Main router created and added at App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const router = require('./routes')
 
 const app = express()
 app.use(cors())
+app.use(router)
 
 module.exports = app
