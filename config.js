dc8895259ea80e244f2fad04fcd85dd1b6e8222e
SylphidZoul <contact@jcruzcardozo.com>
2021-04-10 20:17:06 -0300

    Rick and Morty API's url added at config

--- config.js
+++ config.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 module.exports = {
   API: {
     PORT: process.env.PORT || 3004,
-    ENV: process.env.NODE_ENV || 'production'
+    ENV: process.env.NODE_ENV || 'production',
+    SERVICES: {
+      RICK_AND_MORTY_API: 'https://rickandmortyapi.com/api/'
+    }
   }
 }
