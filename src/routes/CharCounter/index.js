0459b8a952bf2b9fc10b3795fef525e2de644992
Juan Cruz Cardozo <60722288+SylphidZoul@users.noreply.github.com>
2021-04-11 00:35:01 -0300

    Feature/validation middleware (#7)
    * charCounter's query validation created with Joi
    
    * dataValidation's middleware created and added at charCounter's get endpoint
    
    Co-authored-by: SylphidZoul <contact@jcruzcardozo.com>

--- src/routes/CharCounter/index.js
+++ src/routes/CharCounter/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 const { charCounterController } = require('../../controllers')
 const { controllerTryCatchWrapper } = require('../../utils')
+const dataValidation = require('../../middleware/dataValidation')
+const { charCounterQueryValidation } = require('../../utils/Schemas/CharCounter')
 
 router.get(
   '/',
+  dataValidation(charCounterQueryValidation),
   controllerTryCatchWrapper({
     sucessStatusCode: 200,
     errorStatusCode: 500,
