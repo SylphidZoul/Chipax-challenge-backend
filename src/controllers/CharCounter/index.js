98ff651a42d3149cd44a47efa1e584d08020bc2a
Juan Cruz Cardozo <60722288+SylphidZoul@users.noreply.github.com>
2021-04-10 23:09:29 -0300

    Feature/char counter controller (#5)
    * CharCounter util created
    
    * CharCounter's controller finished
    
    Co-authored-by: SylphidZoul <contact@jcruzcardozo.com>

--- src/controllers/CharCounter/index.js
+++ src/controllers/CharCounter/index.js
@@ -1,10 +1,30 @@
-const getCounts = async (req, res) => {
-  return {
-    lettersCInCharacters: Math.floor(Math.random() * 1000),
-    lettersEInEpisodes: Math.floor(Math.random() * 1000),
-    lettersLInLocations: Math.floor(Math.random() * 1000),
-    elapsedTime: `${Math.floor(Math.random() * 1000)} ms.`
+const { rickAndMortyAPI: { getAllRickAndMortyAPIData } } = require('../../services/')
+const { charCounter } = require('../../utils')
+
+const getCounts = async (req) => {
+  const timeStart = process.hrtime()
+  const requiredCharCountPerEndpoint = {
+    character: req.query.character_letter || 'c',
+    episode: req.query.episode_letter || 'e',
+    location: req.query.location_letter || 'l'
   }
+
+  const endpoints = Object.keys(requiredCharCountPerEndpoint)
+
+  const countListPromise = endpoints
+    .map(async (endpoint) => {
+      const characterToCount = requiredCharCountPerEndpoint[endpoint]
+      const endpointData = await getAllRickAndMortyAPIData(endpoint)
+      const count = charCounter(endpointData, characterToCount)
+      return { endpoint, characterToCount, count }
+    })
+
+  const countList = await Promise.all(countListPromise)
+
+  const [seconds, nanoseconds] = process.hrtime(timeStart)
+  const executionTime = `${seconds}s ${Math.floor(nanoseconds / 1000000)}ms`
+
+  return { countList, executionTime }
 }
 
 module.exports = {
