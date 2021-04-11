7c74ecba3f609903d6e98bc13e26618ecce0d03b
SylphidZoul <contact@jcruzcardozo.com>
2021-04-11 00:31:30 -0300

    charCounter's query validation created with Joi

---
+++ src/utils/Schemas/CharCounter/index.js
@@ -0,0 +1,12 @@
+const Joi = require('joi')
+
+const letter = Joi.string().pattern(/^[a-z]{1}$/)
+const charCounterQueryValidation = Joi.object({
+  character_letter: letter,
+  episode_letter: letter,
+  location_letter: letter
+})
+
+const letterErrMessage = 'The letter query must be a single character beetween a-z.'
+
+module.exports = { charCounterQueryValidation, letterErrMessage }
