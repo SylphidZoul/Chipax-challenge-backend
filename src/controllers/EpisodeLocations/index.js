91a48a6ffafa03a1d5ff82549f71a406d5e9ce05
Juan Cruz Cardozo <60722288+SylphidZoul@users.noreply.github.com>
2021-04-10 23:45:00 -0300

    Feature/episode locations controller (#6)
    * GetEpisodeLocations util created
    
    * EpisodeLocations' controller finished
    
    Co-authored-by: SylphidZoul <contact@jcruzcardozo.com>

--- src/controllers/EpisodeLocations/index.js
+++ src/controllers/EpisodeLocations/index.js
@@ -1,8 +1,25 @@
-const getLocationsPerEpisode = async (req, res) => {
-  return {
-    episodes: [],
-    elapsedTime: `${Math.floor(Math.random() * 1000)} ms.`
-  }
+const { rickAndMortyAPI: { getAllRickAndMortyAPIData } } = require('../../services/')
+const { getEpisodeLocations } = require('../../utils')
+
+const getLocationsPerEpisode = async () => {
+  const timeStart = process.hrtime()
+  const listsPromises = [
+    getAllRickAndMortyAPIData('episode'), getAllRickAndMortyAPIData('character')
+  ]
+  const [episodesList, charactersList] = await Promise.all(listsPromises)
+
+  const locationsByCharacterId = charactersList.reduce((dic, character) => {
+    dic[character.id] = character.origin.name
+    return dic
+  }, {})
+
+  const episodeLocations = episodesList
+    .map(episode => getEpisodeLocations(episode, locationsByCharacterId))
+
+  const [seconds, nanoseconds] = process.hrtime(timeStart)
+  const executionTime = `${seconds}s ${Math.floor(nanoseconds / 1000000)}ms`
+
+  return { episodeLocations, executionTime }
 }
 
 module.exports = {
