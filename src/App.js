922e6f17a6cb3e3f684826fe1c037c44f8998429
SylphidZoul <contact@jcruzcardozo.com>
2021-04-14 15:10:45 -0300

    NotFoundResponse and express' json middleware added at App, gitkeeps files deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes')
+const { notFoundResponse } = require('./middleware')
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 app.use(router)
+app.use(notFoundResponse)
 
 module.exports = app
